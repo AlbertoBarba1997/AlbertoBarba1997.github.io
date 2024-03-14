d87e2e7971030708e059627e0b88ff291286dca5
AlbertoBarba1997 <juanalbertobarba1997@gmail.com>
2024-03-14 13:37:15 +0100

    1. Navbar animada header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Componentes principales
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+
+//Contenido individual
+import { AboutComponent } from './components/about/about.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'tecnologias', component: TecnologiasComponent},
+  { path: 'contacto', component: ContactoComponent},
+  { path: 'proyectos', component: ProyectosComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { PortadaComponent } from './components/portada/portada.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    FooterComponent,
+    SobremiComponent,
+    AboutComponent,
+    HomeComponent,
+    PortadaComponent,
+    TecnologiasComponent,
+    ProyectosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
