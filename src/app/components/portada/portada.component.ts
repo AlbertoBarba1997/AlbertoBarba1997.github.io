f19743a960ed171f98e92b3ec3d57487f4ebff15
AlbertoBarba1997 <juanalbertobarba1997@gmail.com>
2024-03-20 22:34:26 +0100

    3. Portada!

--- src/app/components/portada/portada.component.ts
+++ src/app/components/portada/portada.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription, interval } from 'rxjs';
+import { map, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-portada',
   templateUrl: './portada.component.html',
   styleUrls: ['./portada.component.css']
 })
-export class PortadaComponent {
+export class PortadaComponent implements OnInit {
+  text = "Java Developer | Experienced in large-scale team projects.";
+  typewriterText = "";
+  index = 0;
+  speed = 35;
+  intervalSubscription: Subscription | null = null;
 
+  ngOnInit(): void {
+    this.intervalSubscription = interval(this.speed).pipe(
+      map(() => this.index++),
+      takeWhile(() => this.index <= this.text.length)
+    ).subscribe(() => {
+      this.typewriterText = this.text.slice(0, this.index);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalSubscription) {
+      this.intervalSubscription.unsubscribe();
+    }
+  }
 }
