16920c90641a94e9729e4a0202d927a0b1a36942
AlbertoBarba1997 <juanalbertobarba1997@gmail.com>
2024-03-25 22:34:55 +0100

    11. contacto

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+  mostrarAlerta: boolean = false;
 
+  copiarCorreo(inputElement: HTMLInputElement) {
+    inputElement.select();
+    navigator.clipboard.writeText(inputElement.value)
+      .then(() => {
+        this.mostrarAlerta = true;
+        setTimeout(() => {
+          this.mostrarAlerta = false;
+          inputElement.blur(); // Desenfocar el input
+        }, 1500); // 3000 milliseconds = 3 segundos
+      })
+      .catch(err => {
+        console.error('Error al copiar el correo:', err);
+      });
+  }
+
+  cerrarAlerta() {
+    this.mostrarAlerta = false;
+  }
 }
